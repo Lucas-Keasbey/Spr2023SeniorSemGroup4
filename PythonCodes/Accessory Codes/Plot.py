8dc809b9cf046e8ae51ecc770857df2d6f634747
Walker Hayes <112342932+wthayes@users.noreply.github.com>
2023-04-27 17:31:19 -0400

    More Docstring formatting

--- PythonCodes/Accessory Codes/Plot.py
+++ PythonCodes/Accessory Codes/Plot.py
@@ -35,19 +35,19 @@ files = [""] * numTrials
 #     files[i] = filePrefix + "Trial%d.txt"%(i+150)
 
 # Data from basic, linear, and cnn models: LR: 0.01, Epochs: 25
-# files[0] = filePrefix + "Trial140.txt"
+files[0] = filePrefix + "Trial140.txt"
 # files[1] = filePrefix + "Trial150.txt"
 # files[2] = filePrefix + "Trial162.txt"
 
 # Data from a specific trial of 150 epochs
-files[0] = filePrefix + "Trial165.txt"
+# files[0] = filePrefix + "Trial165.txt"
     
 
 print("Loading Plotter")
 
 Plot = DataPlotter(files)
 
-Plot.plotLoss(165)
+Plot.plotLoss(140)
 
 Plot.plotAcc()
 
